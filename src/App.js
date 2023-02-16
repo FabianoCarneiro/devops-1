fce5a25ae0010e68ec26772fcbd7b740ea4cf039
Guilherme Henrique de Melo <33105000+ghenmelo@users.noreply.github.com>
2023-02-16 20:59:45 -0300

    Arquivos projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Consegui subir o projeto com Github Actions</h1>
+    </div>
+  );
+}
+
+export default App;
