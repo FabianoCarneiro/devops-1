7987f9b939ac5f98f575f99e071ea65e6cee6e60
Guilherme Henrique de Melo <33105000+ghenmelo@users.noreply.github.com>
2023-02-16 22:22:27 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Consegui subir o projeto com Github Actions</h1>
+      <h1>Consegui subir o projeto com Github Actions, Chupa Roxo</h1>
     </div>
   );
 }
